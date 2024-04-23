2d1d2556043ffd86eadcec61cf4a96bcadab1977
Evan Dougherty <doughertyevan7@gmail.com>
2024-04-23 17:42:31 -0400

    intial commit

---
+++ TicTacToeGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class TicTacToeGUI extends JFrame
+{
+    public TicTacToeGUI()
+    {
+        super();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
